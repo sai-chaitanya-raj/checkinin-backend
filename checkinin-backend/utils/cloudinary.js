7b18ee9c0f83727cda979d2234a6c1a8e74884a8
R1j <chaitanyaraj809@gmail.com>
2026-02-23 21:33:38 +0530

    Updated Profile sectionby connectiong to cloudinary

--- checkinin-backend/utils/cloudinary.js
+++ checkinin-backend/utils/cloudinary.js
@@ -1,11 +1,12 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
+const config = require("../config");
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: config.cloudinary.cloudName,
+    api_key: config.cloudinary.apiKey,
+    api_secret: config.cloudinary.apiSecret,
 });
 
 const storage = new CloudinaryStorage({
